dcf6497711ff2989e98944ea5527837600a39b55
Namr2789 <83170416+Namr2789@users.noreply.github.com>
2021-10-02 10:13:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menu_list_array = ["Veg Margherita Pizza","Chicken Deluxe Pizza","Non Veg Supreme","Chicken Loaaded Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata="<class = menu_list>"
+menu_list_array.sort()
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+
+
+}
+
+function add_top(){
+
+}
